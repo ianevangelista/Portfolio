865d8a668e8ac3c963a83e5879fff19fe70d0c2c
ianevangelista <ianevangelista1999@gmail.com>
2020-02-01 16:59:34 +0100

    Added projects

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div className={"card w-" + this.props.cardSize + " m-3 border-0"}>
+    <div className={"card w-" + props.cardSize + " m-3 border-0"}>
       <div className="card-body p-0">
-        <div className="m-0">{this.props.children}</div>
+        <div className="m-0">{props.children}</div>
       </div>
     </div>
   );
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../Styles/Projects.css";
+import ProjectCard from "../Components/ProjectCard";
+
 export default function Projects() {
   return (
     <div>
@@ -8,14 +10,7 @@ export default function Projects() {
           <h1 className="text-dark my-3">PROSJEKTER</h1>
           <div className="row">
             <div className="col">
-              <i class="fa fa-envelope"></i>
-              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=ianevangelista1999@gmail.com&su=Hendvendelse">
-                <p>ianevangelista1999@gmail.com</p>
-              </a>
-            </div>
-            <div className="col">
-              <i class="fa fa-phone"></i>
-              <p>+47 45049804</p>
+              <ProjectCard />
             </div>
           </div>
         </div>
@@ -23,4 +18,3 @@ export default function Projects() {
     </div>
   );
 }
-//isbil, monte, quiz, nyhet, portfolio
