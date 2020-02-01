8e271e731dd178b05dcf82fca3e1ad5d664ad941
ianevangelista <ianevangelista1999@gmail.com>
2020-02-01 13:02:43 +0100

    Updates

---
+++ precache-manifest.4b60900789f63b2502fcf33350650357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b4e2d25e1d45726c29a411df728a5e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3fad53277a3dac982770",
+    "url": "/Portfolio/static/css/main.40c11d9e.chunk.css"
+  },
+  {
+    "revision": "4d1afa9f3a62bc9c3163",
+    "url": "/Portfolio/static/js/2.dedba19e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Portfolio/static/js/2.dedba19e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fad53277a3dac982770",
+    "url": "/Portfolio/static/js/main.5a05ff8b.chunk.js"
+  },
+  {
+    "revision": "fc5338d523801bf5e718",
+    "url": "/Portfolio/static/js/runtime-main.4005ac6c.js"
+  },
+  {
+    "revision": "67980e792bc10eb32d0226e466a6fe21",
+    "url": "/Portfolio/static/media/cv.67980e79.pdf"
+  },
+  {
+    "revision": "72920b94323403392e71b56064b166aa",
+    "url": "/Portfolio/static/media/ian.72920b94.jpg"
+  }
+]);
